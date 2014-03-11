279142a7d71d3c6270883bb41d602b03b7acb2e5
Michael G. Noll <mnoll@verisign.com>
2014-03-11 18:23:46 +0100

    Properly create $data_dir recursively

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,6 +5,13 @@ class zookeeper::install inherits zookeeper {
     name    => $package_name,
   }
 
+  # This exec ensures we create intermediate directories for $data_dir as required
+  exec { 'create-zookeeper-data-directory':
+    command => "mkdir -p ${data_dir}",
+    path    => ['/bin', '/sbin'],
+    require => Package['zookeeper-server'],
+  }
+  ->
   file { $data_dir:
     ensure       => directory,
     owner        => $user,
